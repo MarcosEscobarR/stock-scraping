42f08babbe72912f79a4054cabf788b316f25a4f
MarcosEscobarR <marcosmaer600@gmail.com>
2022-05-12 11:35:44 -0400

    fininshed data consuming

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import {chromium} from 'playwright'
+
+(async () => {
+   const browser = await chromium.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.stock.com.py/Ofertas.aspx');
+
+    const categories =  await page.locator(' .caja-categoria a');
+    const categoriesCount = await categories.count();
+    for (let i = 0; i < categoriesCount; i ++) {
+        await categories.nth(i).click();
+
+        var items = await page.locator('.product-item')
+        console.log(await categories.nth(i).innerText(),  {item: await items.allInnerTexts()})
+    }
+
+    await browser.close();
+})();
