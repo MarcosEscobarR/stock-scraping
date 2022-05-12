0a6960ebb68f8a8cb834987789de6e063a7961f8
MarcosEscobarR <marcosmaer600@gmail.com>
2022-05-12 12:23:54 -0400

    fix some bugs

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,17 @@ import {chromium} from 'playwright'
 
 (async () => {
    const browser = await chromium.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.stock.com.py/Ofertas.aspx');
-
-    const categories =  await page.locator(' .caja-categoria a');
-    const categoriesCount = await categories.count();
-    for (let i = 0; i < categoriesCount; i ++) {
-        await categories.nth(i).click();
-
-        var items = await page.locator('.product-item')
-        console.log(await categories.nth(i).innerText(),  {item: await items.allInnerTexts()})
-    }
+    // const page = await browser.newPage();
+    // await page.goto('https://www.stock.com.py/Ofertas.aspx');
+    //
+    // const categories =  await page.locator(' .caja-categoria a');
+    // const categoriesCount = await categories.count();
+    // for (let i = 0; i < categoriesCount; i ++) {
+    //     await categories.nth(i).click();
+    //
+    //     var items = await page.locator('.product-item')
+    //     console.log(await categories.nth(i).innerText(),  {item: await items.allInnerTexts()})
+    // }
 
     await browser.close();
 })();
